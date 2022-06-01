3d3834f94dfc024ec9a81b566c1c1f612cabf915
VadymMelnyk21 <91023743+VadymMelnyk21@users.noreply.github.com>
2022-06-02 00:09:26 +0300

    додавання відгуків

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,21 +6,35 @@ export class App extends Component {
   neutral: 0,
   bad: 0
   }
+
+  onIncrementGood = () => {
+    this.setState(prev => {return { good: prev.good +1,} })
+ 
+  } 
+
+  onIncrementNeutral = () => {
+    this.setState(prev => {return { neutral: prev.neutral +1,} })
+  }
+  
+  onIncrementBad = () => {
+    this.setState(prev => {return { bad: prev.bad +1,} })
+  } 
   
   render() {
+    const { good, neutral, bad } = this.state;
     return (
       <section>
         <p>Please leave feedback</p>
         <div>
-          <button type="botton" onClick={''}>Good</button>
-          <button type="botton" onClick={''}>Neutral</button>
-          <button type="botton" onClick={''}>Bad</button>
+          <button type="botton" onClick={this.onIncrementGood}>Good</button>
+          <button type="botton" onClick={this.onIncrementNeutral}>Neutral</button>
+          <button type="botton" onClick={this.onIncrementBad}>Bad</button>
         </div>
         <div>
           <p>Statistics</p>
-          <p>Good: </p>
-          <p>Neutral: </p>
-          <p>Bad: </p>
+          <p>Good:{good} </p>
+          <p>Neutral: {neutral} </p>
+          <p>Bad: {bad} </p>
       </div>
       </section>
     )
